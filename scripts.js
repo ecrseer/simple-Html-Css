88b80172eff7b68a0373e89167018bee072d75d3
Gabriel Justino <60169516+ecrseer@users.noreply.github.com>
2020-11-10 19:00:47 -0300

    backgroundChanger

--- scripts.js
+++ scripts.js
@@ -1,4 +1,10 @@
+let isWhite = false;
+
 function changeTheme(){
     
+    isWhite ? document.querySelector("body").style.backgroundColor='#010101' :
+    document.querySelector("body").style.backgroundColor='#fff';
+
+    isWhite= !isWhite;
 
 }
